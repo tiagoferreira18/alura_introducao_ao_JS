8b4840f3a72787c1969b05f90fdb973102ff98b3
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-12-05 15:51:52 -0300

    Continuação das aulas

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let titulo =  document.querySelector('.titulo');
+titulo.textContent = 'Tiago Nutricionista'
+
+let paciente = document.querySelector('#primeiro-paciente');
+
+let tdPeso = paciente.querySelector('.info-peso');
+let peso = tdPeso.textContent;
+
+
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
