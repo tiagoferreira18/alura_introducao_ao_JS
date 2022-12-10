c6b8eaa3bda73d92cfbefb6c1efb8fe717f14026
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-12-10 20:03:57 -0300

    fim de modulo

--- js/main.js
+++ js/main.js
@@ -14,11 +14,36 @@ console.log(paciente); //tr
 console.log(tdPeso); // td que tem o peso
 console.log(altura); // obter 100
 
-let imc = peso / (altura*altura);
+
+let pesoEhValido = true;
+let alturaEhValida = true;
 
 
 //Inserindo o IMC dinamicamente
 let infoImc = paciente.querySelector('.info-imc');
-infoImc.textContent = imc;
+
+
+if(peso <= 0 || peso >= 1000){
+    console.log('Peso invalido');
+    pesoEhValido = false;
+    infoImc.textContent = `Peso Inválido`;
+}
+
+if(altura <= 0 || altura >= 3.00){
+    console.log('Altura inválida');
+    alturaEhValida = false;
+    infoImc.textContent = `altura Inválido`;
+
+}
+
+
+
+if(pesoEhValido && alturaEhValida){
+    let imc = peso / (altura*altura);
+    infoImc.textContent = imc;
+
+}
+
+
 
 
