95813e8b551b90da724166705d39b72d1676eca0
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-12-10 14:41:46 -0300

    calculo imc

--- js/main.js
+++ js/main.js
@@ -6,7 +6,19 @@ let paciente = document.querySelector('#primeiro-paciente');
 let tdPeso = paciente.querySelector('.info-peso');
 let peso = tdPeso.textContent;
 
+let tdAltura = paciente.querySelector('.info-altura');
+let altura = tdAltura.textContent;
 
-console.log(paciente);
-console.log(tdPeso);
-console.log(peso);
+
+console.log(paciente); //tr
+console.log(tdPeso); // td que tem o peso
+console.log(altura); // obter 100
+
+let imc = peso / (altura*altura);
+
+
+//Inserindo o IMC dinamicamente
+let infoImc = paciente.querySelector('.info-imc');
+infoImc.textContent = imc;
+
+
