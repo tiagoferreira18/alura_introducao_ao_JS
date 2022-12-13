2523965eac978cd7bc0de2f8b888220962f6bd7d
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-12-13 15:41:33 -0300

    salvando aulas

--- js/main.js
+++ js/main.js
@@ -1,49 +1,59 @@
 let titulo =  document.querySelector('.titulo');
 titulo.textContent = 'Tiago Nutricionista'
 
-let paciente = document.querySelector('#primeiro-paciente');
-
-let tdPeso = paciente.querySelector('.info-peso');
-let peso = tdPeso.textContent;
-
-let tdAltura = paciente.querySelector('.info-altura');
-let altura = tdAltura.textContent;
 
 
-console.log(paciente); //tr
-console.log(tdPeso); // td que tem o peso
-console.log(altura); // obter 100
+let pacientes = document.querySelectorAll('.paciente');
 
+for (let i = 0; i < pacientes.length; i++) {
+    
+    let tdPeso = pacientes[i].querySelector('.info-peso');
+    let peso = tdPeso.textContent;
+    
+    let tdAltura = pacientes[i].querySelector('.info-altura');
+    let altura = tdAltura.textContent;
+    
+    
+    
+    let pesoEhValido = true;
+    let alturaEhValida = true;
+    
+    
+    //Inserindo o IMC dinamicamente
+    let infoImc = pacientes[i].querySelector('.info-imc');
+    
+       
+    if(peso <= 0 || peso >= 1000){
+        console.log('Peso invalido');
+        pesoEhValido = false;
+        infoImc.textContent = `Peso Inválido`;
+        pacientes[i].classList.add('paciente-invalido');
 
-let pesoEhValido = true;
-let alturaEhValida = true;
-
-
-//Inserindo o IMC dinamicamente
-let infoImc = paciente.querySelector('.info-imc');
-
-
-if(peso <= 0 || peso >= 1000){
-    console.log('Peso invalido');
-    pesoEhValido = false;
-    infoImc.textContent = `Peso Inválido`;
+    }
+    
+    if(altura <= 0 || altura >= 3.00){
+        console.log('Altura inválida');
+        alturaEhValida = false;
+        infoImc.textContent = `altura Inválido`;
+        pacientes[i].classList.add('paciente-invalido');
+    
+    }
+        
+    
+    if(pesoEhValido && alturaEhValida){
+        let imc = peso / (altura*altura);
+        infoImc.textContent = imc.toFixed(2);
+    
+    }    
+      
 }
+    
 
-if(altura <= 0 || altura >= 3.00){
-    console.log('Altura inválida');
-    alturaEhValida = false;
-    infoImc.textContent = `altura Inválido`;
+let botaoAdicionar = document.querySelector('#adicionar-paciente');
 
+botaoAdicionar.addEventListener("click", btn);
+
+function btn(event){
+    event.preventDefault()
+    console.log('fui clicado, está funcionando');
 }
-
-
-
-if(pesoEhValido && alturaEhValida){
-    let imc = peso / (altura*altura);
-    infoImc.textContent = imc;
-
-}
-
-
-
-
